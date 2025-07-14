7912f3df0bdf548683e2ab42ce7537d273ecf55c
Tim Nagorskikh <tim@waretronix.com>
2025-07-14 10:53:39 +0300

    Update

--- Debian-based/Server/install-gh-docker.sh
+++ Debian-based/Server/install-gh-docker.sh
@@ -1,4 +1,3 @@
-#!/bin/bash 
 #USAGE: wget https://github.com/trvedxemon/quick-setup/blob/main/Debian-based/Server/install-gh-docker.sh && chmod +x install-gh-docker.sh && sudo ./install-gh-docker.sh
 
 (type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
@@ -10,19 +9,22 @@
 	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 	&& sudo apt update \
 	&& sudo apt install gh -y
+
 # Add Docker's official GPG key:
 sudo apt-get update
-    &&sudo apt-get install ca-certificates curl
-    &&sudo install -m 0755 -d /etc/apt/keyrings
-    &&sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    &&sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    &&sudo apt-get update
-    &&sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Post-install login
 
